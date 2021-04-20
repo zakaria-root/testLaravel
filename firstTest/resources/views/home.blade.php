41fd0420fb1740521cec25bec20d1f913c778833
zakaria-root <elmourtazakezia@gmail.com>
2021-04-20 21:39:11 +0000

    modifier l home page par unphoto et peu d'ecriture

--- firstTest/resources/views/home.blade.php
+++ firstTest/resources/views/home.blade.php
@@ -1,24 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 p-5">
+                <img src="https://instagram.fcmn2-2.fna.fbcdn.net/v/t51.2885-19/s320x320/175668775_201357994892496_4670044011755257933_n.jpg?tp=1&_nc_ht=instagram.fcmn2-2.fna.fbcdn.net&_nc_ohc=iMisQe0PiCMAX8YUxoA&edm=ALwy07oAAAAA&ccb=7-4&oh=9e96d232ad4c0f19a95f35d0ce376bf3&oe=60A5611B&_nc_sid=261c40" style="max-height:200px" class="rounded-circle">
+            </div>
+            <div class="col-md-9 p-5 ">
+                <h1>tanjiro.__.kun</h1>
+                <div class="d-flex">
+                    <div class="pr-5"><string>28</string> publications</div>
+                    <div class="pr-5"><string>54</string> abonnés</div>
+                    <div class="pr-5"><string>71</string> abonnements</div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
 @endsection
