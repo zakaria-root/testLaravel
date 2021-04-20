38dca4ea9cee1dfa0b2ca3340ff2e29d886d645c
zakaria-root <elmourtazakezia@gmail.com>
2021-04-20 21:52:29 +0000

    modificationde font pour ajouter les font bold plus des pading in page

--- firstTest/resources/views/home.blade.php
+++ firstTest/resources/views/home.blade.php
@@ -8,11 +8,11 @@
                 <img src="https://instagram.fcmn2-2.fna.fbcdn.net/v/t51.2885-19/s320x320/175668775_201357994892496_4670044011755257933_n.jpg?tp=1&_nc_ht=instagram.fcmn2-2.fna.fbcdn.net&_nc_ohc=iMisQe0PiCMAX8YUxoA&edm=ALwy07oAAAAA&ccb=7-4&oh=9e96d232ad4c0f19a95f35d0ce376bf3&oe=60A5611B&_nc_sid=261c40" style="max-height:200px" class="rounded-circle">
             </div>
             <div class="col-md-9 p-5 ">
-                <h1>tanjiro.__.kun</h1>
+                <h1 class="pb-3">tanjiro.__.kun</h1>
                 <div class="d-flex">
-                    <div class="pr-5"><string>28</string> publications</div>
-                    <div class="pr-5"><string>54</string> abonnés</div>
-                    <div class="pr-5"><string>71</string> abonnements</div>
+                    <div class="pr-5"><strong>28</strong> publications</div>
+                    <div class="pr-5"><strong>54</strong> abonnés</div>
+                    <div class="pr-5"><strong>71</strong> abonnements</div>
                 </div>
             </div>
         </div>
