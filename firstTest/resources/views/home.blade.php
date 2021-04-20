4a34afcf4208cbc352e98d090a8405c3c0dd4b91
zakaria-root <elmourtazakezia@gmail.com>
2021-04-20 22:26:44 +0000

    ajouter 3 image dans la page home

--- firstTest/resources/views/home.blade.php
+++ firstTest/resources/views/home.blade.php
@@ -5,16 +5,35 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3 p-5">
-                <img src="https://instagram.fcmn2-2.fna.fbcdn.net/v/t51.2885-19/s320x320/175668775_201357994892496_4670044011755257933_n.jpg?tp=1&_nc_ht=instagram.fcmn2-2.fna.fbcdn.net&_nc_ohc=iMisQe0PiCMAX8YUxoA&edm=ALwy07oAAAAA&ccb=7-4&oh=9e96d232ad4c0f19a95f35d0ce376bf3&oe=60A5611B&_nc_sid=261c40" style="max-height:200px" class="rounded-circle">
+                <img src="https://instagram.fcmn2-2.fna.fbcdn.net/v/t51.2885-19/s320x320/175668775_201357994892496_4670044011755257933_n.jpg?tp=1&_nc_ht=instagram.fcmn2-2.fna.fbcdn.net&_nc_ohc=iMisQe0PiCMAX8YUxoA&edm=ALwy07oAAAAA&ccb=7-4&oh=9e96d232ad4c0f19a95f35d0ce376bf3&oe=60A5611B&_nc_sid=261c40" style="max-height:170px" class="rounded-circle">
             </div>
             <div class="col-md-9 p-5 ">
                 <h1 class="pb-3">tanjiro.__.kun</h1>
                 <div class="d-flex">
-                    <div class="pr-5"><strong>28</strong> publications</div>
-                    <div class="pr-5"><strong>54</strong> abonnés</div>
-                    <div class="pr-5"><strong>71</strong> abonnements</div>
+                    <div class="pr-5"><strong>28 </strong> publications</div>
+                    <div class="pr-5"><strong>54 </strong> abonnés</div>
+                    <div class="pr-5"><strong>71 </strong> abonnements</div>
+                </div>
+                <div class="mt-3">
+                    <strong>Elmourtazak zakaria</strong> <br>
+                    W E L C O M E T O M Y W O R L D😉 <br>
+                    🔸Editing <br>
+                    🔸coding <br>
+                    🔸anime <br>
+                    🔸and me... 🙂<br>
+                    ◆◇◈ ◆◇<br>
+                    🔹if you want to learn📚 amv editing: <br>
+                </div>
+                <div class="font-weight-bold ">
+                    <a href="https://l.instagram.com/?u=https%3A%2F%2Fwww.youtube.com%2Fchannel%2FUCUY5I0xV7ATsl9srfQsaOTQ%2Ffeatured%3Fdisable_polymer%3D1&e=ATPz6uuWUn8g8AOtqxtQON4KaLJql90bom0LovpHJlUHoh4vTnN7zqlM3bxEVVGxr9IUvPrxXsPOo8R9xMgau9g&s=1">www.youtube.com/channel/UCUY5I0xV7ATsl9srfQsaOTQ/featured?disable_polymer=1...
+                    </a>
                 </div>
             </div>
+                <div class="row pt-5">
+                    <div class="col-md-4"><img src="https://instagram.fcmn2-1.fna.fbcdn.net/v/t51.2885-15/e35/s480x480/160368115_104029401721539_6093841033454184369_n.jpg?tp=1&_nc_ht=instagram.fcmn2-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=HYJ1Wznc_9QAX_4mdps&edm=ABfd0MgAAAAA&ccb=7-4&oh=3112655b54c47233f7b2427af0b6035a&oe=608175D0&_nc_sid=7bff83" class="w-100"></div>
+                    <div class="col-md-4"><img src="https://instagram.fcmn2-1.fna.fbcdn.net/v/t51.2885-15/e35/s480x480/159472132_1210727819360776_2851936615475364228_n.jpg?tp=1&_nc_ht=instagram.fcmn2-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=cdTqO8T94k0AX-9zlJ2&edm=ABfd0MgAAAAA&ccb=7-4&oh=8d26710f0a74c92453d8ad86393f42b2&oe=608183C6&_nc_sid=7bff83" class="w-100"></div>
+                    <div class="col-md-4"><img src="https://instagram.fcmn2-1.fna.fbcdn.net/v/t51.2885-15/e35/c140.0.360.360a/159259194_439952007263727_4049572572351658877_n.jpg?tp=1&_nc_ht=instagram.fcmn2-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=jIO7uVkVxYMAX9NxQeH&edm=ABfd0MgAAAAA&ccb=7-4&oh=5dfaf46b254776569ace4724fb460098&oe=60815BE2&_nc_sid=7bff83" class="w-100"></div>
+                </div>
         </div>
     </div>
 
